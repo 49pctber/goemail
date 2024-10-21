03c72a7d9028da09416307fb9b4d435a022c09db
Bryan Redd <bryan.d.redd@gmail.com>
2024-10-21 11:24:37 -0600

    Updated usage string

--- cmd/goemail/goemail.go
+++ cmd/goemail/goemail.go
@@ -9,6 +9,26 @@ import (
 )
 
 func main() {
+	var to, subject, body string
+	var html bool
+
+	flag.StringVar(&to, "to", "", "recipient's email address")
+	flag.StringVar(&subject, "subject", "", "subject of message")
+	flag.StringVar(&body, "body", "", "the body of the message")
+	flag.BoolVar(&html, "html", false, "indicate body contains html")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), "\nBe sure to set the EMAIL_NAME, EMAIL_ADDRESS, and EMAIL_PASSWORD environment variables.\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  EMAIL_NAME is the friendly name to display to the recipient (e.g. John Doe)\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  EMAIL_ADDRESS is email address from which you will send the email\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  EMAIL_PASSWORD is used to authenticate EMAIL_ADDRESS\n")
+	}
+
+	flag.Parse()
+
+	valid := true
+
 	err := goemail.Configure(
 		os.Getenv("EMAIL_NAME"),
 		os.Getenv("EMAIL_ADDRESS"),
@@ -18,18 +38,6 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	var to, subject, body string
-	var html bool
-
-	flag.StringVar(&to, "to", "", "recipient's email address")
-	flag.StringVar(&subject, "subject", "", "subject of message")
-	flag.StringVar(&body, "body", "", "the body of the message")
-	flag.BoolVar(&html, "html", false, "indicate body contains html")
-
-	flag.Parse()
-
-	valid := true
 
 	if len(to) == 0 {
 		fmt.Println("specify recipient using -to flag")
